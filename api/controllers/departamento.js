028bde28743e605721f70fff24f9553ae39214cf
karuchevez <30363204+karuchevez@users.noreply.github.com>
2017-08-25 00:09:17 -0600

    creacion archivos departamento y empleados rout y controller

---
+++ api/controllers/departamento.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var ora = require('../models/ora');
+
+exports.enlistar_todo = function(req,res){
+
+  ora.then(function(con){
+    con.execute('select * from departamento',function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al obtener todo departamento " + err.message
+        }));
+      }else{
+        console.log("departamento respondio :" + result.rows);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rows));
+      }
+    });
+  }); //fin del ora.then()
+};
+
+exports.crear_registro = function(req,res){
+  ora.then(function(con){
+    con.execute("INSERT INTO departamento VALUES(:in_coddepartamento,:in_codsucursal,:in_nombredepartamento,:in_numplazas)",
+    [req.body.in_coddepartamento,req.body.in_codsucursal,req.body.in_nombredepartamento,req.body.in_numplazas],
+    function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al crear un registro departamento" + err.message
+        }));
+      }else{
+        console.log("departamento respondio :" + result.rows);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rowsAffected));
+      }
+    });
+  }); //fin del ora.then()
+};
+exports.obtener_registro = function(req,res){
+  ora.then(function(con){
+    con.execute('select * from departamento where coddepartamento = :id',[req.params.id],function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al obtener todo departamento" + err.message
+        }));
+      }else{
+        console.log("departamento respondio :" + result.rows);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rows));
+      }
+    });
+  }); //fin del ora.then()
+};
+exports.actualizar_registro = function(req,res){
+  ora.then(function(con){
+    con.execute("UPDATE departamento  SET :in_codsucursal,:in_nombredepartamento,:in_numplazas WHERE coddepartamento=:id",
+    [req.body.in_nombre,req.body.in_apellido,req.body.in_fechanacimiento,req.body.in_salario,req.params.id],
+    function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al obtener todo departamento " + err.message
+        }));
+      }else{
+        console.log("departamento actualizo :" + result.rowsAffected);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rowsAffected));
+      }
+    });
+  }); //fin del ora.then()
+};
+exports.borrar_registro = function(req,res){
+  ora.then(function(con){
+    con.execute("DELETE FROM departamento WHERE coddepartamento=:id",
+    [req.params.id],
+    function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al obtener todo departamento " + err.message
+        }));
+      }else{
+        console.log("departamento borro :" + result.rowsAffected);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rowsAffected));
+      }
+    });
+  }); //fin del ora.then()
+};
---
+++ api/controllers/empleados.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var ora = require('../models/ora');
+
+exports.enlistar_todo = function(req,res){
+
+  ora.then(function(con){
+    con.execute('select * from empleados',function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al obtener todo empleados" + err.message
+        }));
+      }else{
+        console.log("empleados respondio :" + result.rows);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rows));
+      }
+    });
+  }); //fin del ora.then()
+};
+
+exports.crear_registro = function(req,res){
+  ora.then(function(con){
+    con.execute("INSERT INTO empleados VALUES(:in_codempleado,:in_codjefe,:in_coddepartamento,:in_codsucursal,:in_nombre1,:in_nombre2,:in_apellido1,:in_apellido2,:in_tituloempleado,TO_DATE(:in_fechanacimiento,'YYYY/MM/DD'),:in_generoempleado,:in_estadocivil,:in_documentoidentidad,:in_tipodocumentoident,:nitempleado,:in_isssempleado,:in_nupempleado,:in_nombreafp,:in_numerocuentabanco,:in_nombrebanco,:in_puesto,:in_tiempotrabajo)",
+    [req.body.in_codempleado,req.body.in_codjefe,req.body.in_coddepartamento,req.body.in_codsucursal,req.body.in_nombre1,req.body.in_nombre2,req.body.in_apellido1,req.body.in_apellido2,req.body.in_tituloempleado,req.body.in_fechanacimiento,req.body.in_generoempledao,req.body.in_estadocivil,req.body.in_documentoidentidad,req.body.in_tipodocumentoident,req.body.in_nitempleado,req.body.in_isssempleado,req.body.in_nupempleado,req.body.in_nombreafp,req.body.in_numerocuentabanco,req.body.in_nombreban,req.body.in_puesto,req.body.in_tiempotrabajo]
+    function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al crear un registro nodetest " + err.message
+        }));
+      }else{
+        console.log("nodetesr respondio :" + result.rows);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rowsAffected));
+      }
+    });
+  }); //fin del ora.then()
+};
+exports.obtener_registro = function(req,res){
+  ora.then(function(con){
+    con.execute('select * from nodetest where id = :id',[req.params.id],function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al obtener todo nodetest " + err.message
+        }));
+      }else{
+        console.log("nodetesr respondio :" + result.rows);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rows));
+      }
+    });
+  }); //fin del ora.then()
+};
+exports.actualizar_registro = function(req,res){
+  ora.then(function(con){
+    con.execute("UPDATE nodetest SET nombre=:in_nombre, apellido=:in_apellido, fechanacimiento=TO_DATE(:in_fechanacimiento,'YYYY/MM/DD'), salario=:in_salario WHERE id=:id",
+    [req.body.in_nombre,req.body.in_apellido,req.body.in_fechanacimiento,req.body.in_salario,req.params.id],
+    function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al obtener todo nodetest " + err.message
+        }));
+      }else{
+        console.log("nodetesr actualizo :" + result.rowsAffected);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rowsAffected));
+      }
+    });
+  }); //fin del ora.then()
+};
+exports.borrar_registro = function(req,res){
+  ora.then(function(con){
+    con.execute("DELETE FROM nodetest WHERE id=:id",
+    [req.params.id],
+    function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al obtener todo nodetest " + err.message
+        }));
+      }else{
+        console.log("nodetesr borro :" + result.rowsAffected);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rowsAffected));
+      }
+    });
+  }); //fin del ora.then()
+};
