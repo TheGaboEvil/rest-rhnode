e66c085433fa3b56461bc012b5e57b2427ff62f3
karuchevez <30363204+karuchevez@users.noreply.github.com>
2017-08-29 20:35:44 -0600

    planilla

---
+++ api/controllers/planilla.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var ora = require('../models/ora');
+
+exports.enlistar_todo = function(req,res){
+
+  ora.then(function(con){
+    con.execute('select * from planilla',function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al obtener todo planilla " + err.message
+        }));
+      }else{
+        console.log("planilla respondio :" + result.rows);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rows));
+      }
+    });
+  }); //fin del ora.then()
+};
+
+exports.crear_registro = function(req,res){
+  ora.then(function(con){
+    con.execute("INSERT INTO planilla VALUES(:in_codplanilla,:in_coddepartamento,:in_codsucursal,TO_DATE(:in_fechaini,'YYYY/MM/DD'),TO_DATE(:in_fechafin,'YYYY/MM/DD'),:in_tipoplanilla)",
+    [req.body.in_codplanilla,req.body.in_coddepartamento,req.body.in_codsucursal,req.body.in_fechaini,req.body.in_fechafin,req.body.in_tipoplanilla],
+    function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al crear un registro nodetest " + err.message
+        }));
+      }else{
+        console.log("planilla respondio :" + result.rows);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rowsAffected));
+      }
+    });
+  }); //fin del ora.then()
+};
+exports.obtener_registro = function(req,res){
+  ora.then(function(con){
+    con.execute('select * from planilla where codplanilla = :id',[req.params.id],function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al obtener todo planilla " + err.message
+        }));
+      }else{
+        console.log("planilla respondio :" + result.rows);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rows));
+      }
+    });
+  }); //fin del ora.then()
+};
+exports.actualizar_registro = function(req,res){
+  ora.then(function(con){
+    con.execute("UPDATE planilla SET nombre=:in_nombre, apellido=:in_apellido, fechanacimiento=TO_DATE(:in_fechanacimiento,'YYYY/MM/DD'), salario=:in_salario WHERE id=:id",
+    [req.body.in_nombre,req.body.in_apellido,req.body.in_fechanacimiento,req.body.in_salario,req.params.id],
+    function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al obtener todo nodetest " + err.message
+        }));
+      }else{
+        console.log("planilla actualizo :" + result.rowsAffected);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rowsAffected));
+      }
+    });
+  }); //fin del ora.then()
+};
+exports.borrar_registro = function(req,res){
+  ora.then(function(con){
+    con.execute("DELETE FROM planilla WHERE codplanilla=:id",
+    [req.params.id],
+    function(err,result){
+      if(err){
+        console.log("Error  "+err.message);
+        res.writeHead(500,{'Content-Type':'aplication/json'});
+        res.end(JSON.stringify({
+          status:500,
+          message: "Error al obtener todo planilla " + err.message
+        }));
+      }else{
+        console.log("nodetesr borro :" + result.rowsAffected);
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(result.rowsAffected));
+      }
+    });
+  }); //fin del ora.then()
+};
