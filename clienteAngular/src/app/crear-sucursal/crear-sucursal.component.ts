a314806f09ffa779e30d49a9131f7677bdd24012
ejchicas <ejchicas@gmail.com>
2017-09-01 08:59:46 -0600

    modificaicones al componente form

--- clienteAngular/src/app/crear-sucursal/crear-sucursal.component.ts
+++ clienteAngular/src/app/crear-sucursal/crear-sucursal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {crearSucursal} from './crearSucursal';
 
 @Component({
   selector: 'app-crear-sucursal',
@@ -14,14 +15,14 @@ export class CrearSucursalComponent implements OnInit {
 
   postCrear(in_codsucursal,in_direccion,in_ciudad,in_departamento,in_pais,in_codigopostal,in_telefono,in_coordenadas){
     const datos = {
-      in_codsucursal : this.in_codsucursal;
-      in_direccion : this.in_direccion;
-      in_ciudad : this.in_ciudad;
-      in_departamento : this.in_departamento;
-      in_pais : this.in_pais;
-      in_codigopostal : this.in_codigopostal;
-      in_telefono : this.in_telefono ;
-      in_coordenadas : this.in_coordenadas;
+      in_codsucursal : this.in_codsucursal,
+      in_direccion : this.in_direccion,
+      in_ciudad : this.in_ciudad,
+      in_departamento : this.in_departamento,
+      in_pais : this.in_pais,
+      in_codigopostal : this.in_codigopostal,
+      in_telefono : this.in_telefono ,
+      in_coordenadas : this.in_coordenadas
     } // fin de la constante datos
 
 
