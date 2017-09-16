691aa24999d466b602d018b1bd138c89c360f722
ejchicas <ejchicas@gmail.com>
2017-09-15 21:09:30 -0600

    comit

--- api/routes/planilla.js
+++ api/routes/planilla.js
@@ -6,11 +6,7 @@ module.exports = function(app){
     .get(planilla.enlistar_todo)
     .post(planilla.crear_registro);
 
-<<<<<<< HEAD
-  app.route('/planilla/:id')
-=======
   app.route('/planilla/:id1/:id2')
->>>>>>> 0b464f3b4c36b160f21c5300b09f5e25c4b967a6
   .get(planilla.obtener_registro)
   .put(planilla.actualizar_registro)
   .delete(planilla.borrar_registro);
