d6c6b7d890beffd3c81159be2b69762ed93cc6fd
ejchicas <ejchicas@gmail.com>
2017-09-15 21:47:54 -0600

    fix router

--- api/routes/planillaempleado.js
+++ api/routes/planillaempleado.js
@@ -2,11 +2,11 @@
 
 module.exports = function(app){
   var planillaempledo = require('../controllers/planillaempleado');
-  app.route('/planillaempledo')
+  app.route('/planillaempleado')
     .get(planillaempledo.enlistar_todo)
     .post(planillaempledo.crear_registro);
 
-  app.route('/planillaempledo/:id1/:id2/:id3')
+  app.route('/planillaempleado/:id1/:id2/:id3')
   .get(planillaempledo.obtener_registro)
   .put(planillaempledo.actualizar_registro)
   .delete(planillaempledo.borrar_registro);
