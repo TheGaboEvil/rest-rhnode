60ffa7cade83c550ce5e74b76714d84c98858882
karuchevez <30363204+karuchevez@users.noreply.github.com>
2017-09-27 23:10:04 -0600

    Creacion e servicio adelantoempleado

--- clienteAngular/src/app/adelantosempleados/adelantoempleado.service.ts
+++ clienteAngular/src/app/adelantosempleados/adelantoempleado.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+import { Adelantoempleado } from './adelantoempleado';
 
 @Injectable()
 export class AdelantoempleadoService {
 
-  constructor() { }
+  constructor(private http: Http) {}
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  private URL = 'http://localhost:8030/adelantosempleado';
 
+  crearSucursal(adelantoEmpleado: Adelantoempleado): Promise<Adelantoempleado> {
+    return this.http
+      .post(this.URL, JSON.stringify(adelantoEmpleado), { headers: this.headers })
+      .toPromise()
+      .then(res => res.json().data as Adelantoempleado)
+      .catch(this.handleError);
+  }
+  
+  private handleError(error: any): Promise<any> {
+    console.error('Ha ocurrido un error en Adelantoempleado ', error);
+    return Promise.reject(error.message || error);
+  }
 }
