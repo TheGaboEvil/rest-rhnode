4a9696ec30886edf1c33b1eed3ede98cda1f1d32
ejchicas <ejchicas@gmail.com>
2017-09-27 22:27:01 -0600

    ANOTACIONES

--- clienteAngular/src/app/requerimientosplaza/requerimientoplaza.service.ts
+++ clienteAngular/src/app/requerimientosplaza/requerimientoplaza.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+import { Requerimientosplaza } from './requerimientosplaza';
 
 @Injectable()
 export class RequerimientoplazaService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  private URL = 'http://localhost:8030/requerimientosplaza';
 
+  crearRequerimientoPlaza(requerimientosPlaza: Requerimientosplaza): Promise<Requerimientosplaza> {
+    return this.http
+      .post(this.URL, JSON.stringify(requerimientosPlaza), { headers: this.headers })
+      .toPromise()
+      .then(res => res.json().data as Requerimientosplaza)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('Ha ocurrido un error en Requerimientos Plaza ', error);
+    return Promise.reject(error.message || error);
+  }
 }
--- clienteAngular/src/app/sucursales/sucursal.service.ts
+++ clienteAngular/src/app/sucursales/sucursal.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
+// copias desde aqui
 import { Headers, Http } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
+// hasta aqui
+
+// importar la clase es el archivo nombre.ts
 import { Sucursal } from './sucursal';
 
 @Injectable()
 export class SucursalService {
+
+  // copiar desde aqui
   constructor(private http: Http) {}
   private headers = new Headers({ 'Content-Type': 'application/json' });
   private URL = 'http://localhost:8030/sucursales';
 
+  // Eliminar esta funcion
   getSucursales(): Promise<Sucursal[]> {
     return this.http
       .get(this.URL)
@@ -17,6 +24,12 @@ export class SucursalService {
       .catch(this.handleError);
   }
 
+  /**
+   * Editar los nomre los que inician en minuscula deben iniciar con minuscula
+   *  los que inician con Mayusculas son las Clases
+   *  los que incian con minusculas son instancias de la clase
+   *  TENER MUCHO OJO PARA EDITARLO POR FAVOR....
+   * * */
   crearSucursal(sucursal: Sucursal): Promise<Sucursal> {
     return this.http
       .post(this.URL, JSON.stringify(sucursal), { headers: this.headers })
@@ -25,7 +38,8 @@ export class SucursalService {
       .catch(this.handleError);
   }
   private handleError(error: any): Promise<any> {
-    console.error('Ha ocurrido un error ', error);
+    console.error('Ha ocurrido un error en Sucursal ', error);
     return Promise.reject(error.message || error);
   }
+  // hasta aqui
 }
