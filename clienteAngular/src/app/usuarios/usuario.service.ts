59efb4e8122f2c601a4480fa553c04403baad4e7
ejchicas <ejchicas@gmail.com>
2017-09-30 19:36:53 -0600

    mejoras espero

--- clienteAngular/src/app/usuarios/usuario.service.ts
+++ clienteAngular/src/app/usuarios/usuario.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Usuario } from './usuario';
+import { Http, Headers} from '@angular/http';
 
 
 @Injectable()
 export class UsuarioService {
 
-  constructor() { }
-  crearUsuario(usaurio: Usuario){
+  constructor(private http: Http) {}
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  private URL = 'http://localhost:8030/usuarios';
 
+  crearUsuario(usaurio: Usuario): Promise<Usuario> {
+    return this.http
+      .post(this.URL, JSON.stringify(usaurio), { headers: this.headers })
+      .toPromise()
+      .then(res => res.json().data as Usuario)
+      .catch(this.handleError);
+  }
+  private handleError(error: any): Promise<any> {
+    console.error('Ha ocurrido un error en Sucursal ', error);
+    return Promise.reject(error.message || error);
   }
 }
